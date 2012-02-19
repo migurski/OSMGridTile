dc768b33e39b8c2117d1d672ce0f214fe3027776
Michal Migurski <migurski@li155-150.members.linode.com>
2012-02-19 00:52:54 +0000

    Shoved OSMGrid into a package

--- OSMGrid/__init__.py
+++ OSMGrid/__init__.py
@@ -5,9 +5,9 @@ from json import dump
 from PIL import Image
 from PIL.ImageDraw import ImageDraw
 
-from shapely.wkb import loads
-
 from psycopg2 import connect
+
+from .data import get_landusages_imposm, get_roads_imposm
 
 class GridResponse:
 
@@ -80,7 +80,7 @@ class Provider:
             objects[rgb] = 'road', type, name, id
             
             for geom in getattr(way, 'geoms', [way]):
-                stroke = {18: 5, 17: 3}.get(coord.zoom, 2)
+                stroke = {18: 4, 17: 3}.get(coord.zoom, 2)
                 draw.line(list(geom.coords), fill=rgb, width=stroke)
         
         #
@@ -112,88 +112,3 @@ class Provider:
             grid.append(row)
         
         return GridResponse(img.resize((width, height), Image.NEAREST), [details, grid])
-
-def get_polygons_osm2pgsql(db, bbox, xoff, yoff, xfac, yfac):
-    """
-    """
-    query = """SELECT osm_id, name,
-                      ST_AsBinary(ST_TransScale(way, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS way
-               FROM planet_osm_polygon
-               WHERE way && %(bbox)s
-                 AND ST_Intersects(way, %(bbox)s)
-                 AND osm_id > 0
-               ORDER BY ST_Area(way) DESC""" \
-          % locals()
-    
-    db.execute(query)
-    
-    return [(id, name, loads(str(way_wkb)))
-            for (id, name, way_wkb) in db.fetchall()]
-
-def get_polygons_osm2pgsql(db, bbox, xoff, yoff, xfac, yfac):
-    """
-    """
-    query = """SELECT osm_id, name,
-                      ST_AsBinary(ST_TransScale(way, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS way
-               FROM planet_osm_polygon
-               WHERE way && %(bbox)s
-                 AND ST_Intersects(way, %(bbox)s)
-                 AND osm_id > 0
-               ORDER BY ST_Area(way) DESC""" \
-          % locals()
-    
-    db.execute(query)
-    
-    return [(id, name, loads(str(way_wkb)))
-            for (id, name, way_wkb) in db.fetchall()]
-
-def get_linestrings_osm2pgsql(db, bbox, xoff, yoff, xfac, yfac):
-    """
-    """
-    query = """SELECT osm_id, name,
-                      ST_AsBinary(ST_TransScale(way, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS way
-               FROM planet_osm_line
-               WHERE way && %(bbox)s
-                 AND ST_Intersects(way, %(bbox)s)
-                 AND osm_id > 0
-               ORDER BY ST_Length(way) DESC""" \
-          % locals()
-    
-    db.execute(query)
-    
-    return [(id, name, loads(str(way_wkb)))
-            for (id, name, way_wkb) in db.fetchall()]
-
-def get_landusages_imposm(db, bbox, xoff, yoff, xfac, yfac):
-    """
-    """
-    query = """SELECT osm_id, type, name,
-                      ST_AsBinary(ST_TransScale(the_geom, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS the_geom
-               FROM imposm_oakland_landusages
-               WHERE the_geom && %(bbox)s
-                 AND ST_Intersects(the_geom, %(bbox)s)
-                 AND osm_id > 0
-               ORDER BY z_order ASC, area DESC""" \
-          % locals()
-    
-    db.execute(query)
-    
-    return [(id, type, name, loads(str(way_wkb)))
-            for (id, type, name, way_wkb) in db.fetchall()]
-
-def get_roads_imposm(db, bbox, xoff, yoff, xfac, yfac):
-    """
-    """
-    query = """SELECT osm_id, type, name,
-                      ST_AsBinary(ST_TransScale(the_geom, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS the_geom
-               FROM imposm_oakland_roads
-               WHERE the_geom && %(bbox)s
-                 AND ST_Intersects(the_geom, %(bbox)s)
-                 AND osm_id > 0
-               ORDER BY z_order ASC, ST_Length(the_geom) DESC""" \
-          % locals()
-    
-    db.execute(query)
-    
-    return [(id, type, name, loads(str(way_wkb)))
-            for (id, type, name, way_wkb) in db.fetchall()]
---
+++ OSMGrid/data.py
@@ -0,0 +1,86 @@
+from shapely.wkb import loads
+
+def get_polygons_osm2pgsql(db, bbox, xoff, yoff, xfac, yfac):
+    """
+    """
+    query = """SELECT osm_id, name,
+                      ST_AsBinary(ST_TransScale(way, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS way
+               FROM planet_osm_polygon
+               WHERE way && %(bbox)s
+                 AND ST_Intersects(way, %(bbox)s)
+                 AND osm_id > 0
+               ORDER BY ST_Area(way) DESC""" \
+          % locals()
+    
+    db.execute(query)
+    
+    return [(id, name, loads(str(way_wkb)))
+            for (id, name, way_wkb) in db.fetchall()]
+
+def get_polygons_osm2pgsql(db, bbox, xoff, yoff, xfac, yfac):
+    """
+    """
+    query = """SELECT osm_id, name,
+                      ST_AsBinary(ST_TransScale(way, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS way
+               FROM planet_osm_polygon
+               WHERE way && %(bbox)s
+                 AND ST_Intersects(way, %(bbox)s)
+                 AND osm_id > 0
+               ORDER BY ST_Area(way) DESC""" \
+          % locals()
+    
+    db.execute(query)
+    
+    return [(id, name, loads(str(way_wkb)))
+            for (id, name, way_wkb) in db.fetchall()]
+
+def get_linestrings_osm2pgsql(db, bbox, xoff, yoff, xfac, yfac):
+    """
+    """
+    query = """SELECT osm_id, name,
+                      ST_AsBinary(ST_TransScale(way, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS way
+               FROM planet_osm_line
+               WHERE way && %(bbox)s
+                 AND ST_Intersects(way, %(bbox)s)
+                 AND osm_id > 0
+               ORDER BY ST_Length(way) DESC""" \
+          % locals()
+    
+    db.execute(query)
+    
+    return [(id, name, loads(str(way_wkb)))
+            for (id, name, way_wkb) in db.fetchall()]
+
+def get_landusages_imposm(db, bbox, xoff, yoff, xfac, yfac):
+    """
+    """
+    query = """SELECT osm_id, type, name,
+                      ST_AsBinary(ST_TransScale(the_geom, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS the_geom
+               FROM imposm_oakland_landusages
+               WHERE the_geom && %(bbox)s
+                 AND ST_Intersects(the_geom, %(bbox)s)
+                 AND osm_id > 0
+               ORDER BY z_order ASC, area ASC""" \
+          % locals()
+    
+    db.execute(query)
+    
+    return [(id, type, name, loads(str(way_wkb)))
+            for (id, type, name, way_wkb) in db.fetchall()]
+
+def get_roads_imposm(db, bbox, xoff, yoff, xfac, yfac):
+    """
+    """
+    query = """SELECT osm_id, type, name,
+                      ST_AsBinary(ST_TransScale(the_geom, %(xoff).3f, %(yoff).3f, %(xfac).3f, %(yfac).3f)) AS the_geom
+               FROM imposm_oakland_roads
+               WHERE the_geom && %(bbox)s
+                 AND ST_Intersects(the_geom, %(bbox)s)
+                 AND osm_id > 0
+               ORDER BY z_order ASC, ST_Length(the_geom) ASC""" \
+          % locals()
+    
+    db.execute(query)
+    
+    return [(id, type, name, loads(str(way_wkb)))
+            for (id, type, name, way_wkb) in db.fetchall()]
